2a66440fb7ed5aae60a8f93d80707ca6208f7aa5
Vincent Jim <vjim323@gmail.com>
2014-10-23 13:35:40 -0400

    Created Person class and initialize that collects their name

--- people.rb
+++ people.rb
@@ -1,12 +1,18 @@
 #Exercise 1 - Class Time
 
+class Person
+	def initialize(name)
+		@name = name
+	end
+end
+
 class Student
 	def learn
 		puts "I get it!"
 	end
 end
 
-class Instructer
+class Instructor
 	def teach
 		puts "Everything in Ruby is an object."
 	end
