58a372dd98e74aa597d2eb87fd970841e7907d9b
Vincent Jim <vjim323@gmail.com>
2014-10-23 15:51:31 -0400

    Rover takes input and moves/turns accordingly

--- mars_rover.rb
+++ mars_rover.rb
@@ -1,28 +1,75 @@
 #Exercise 2 - Mars Rover
 
 class Rover
+	attr_accessor :x, :y, :direction
+
 	def initialize(x, y, direction)
 		@x = x
 		@y = y
 		@direction = direction
 	end
+	
+	def display_position
+		puts "#{@x}, #{@y}, #{@direction}"
+	end
 
 	def read_instruction
-		
+		puts "Please enter the instructions for the Rover (M, L, R): "
+		@instructions = gets.chomp
+	end
+
+	def interpret_instructions
+		@instructions.each_char do |c|
+			case c
+			when "M"
+				move
+			when "L"
+				turn_left
+			when "R"
+				turn_right
+			end
+		end
 	end
 
 	def move
-		#new_point = point.x + instruction.x, point.y + instruction.y, point.direction + instruction.direction
+		if (@direction == "N") 
+			@y = @y + 1
+		elsif (@direction == "E") 
+			@x = @x + 1
+		elsif (@direction == "S") 
+			@y = @y - 1
+		else #(@direction == "W") 
+			@x = @x - 1
+		end
 	end
 
-	def turn
-		
+	def turn_left
+		if (@direction == "N") 
+			@direction = "W"
+		elsif (@direction == "W") 
+			@direction = "S"
+		elsif (@direction == "S") 
+			@direction = "E"
+		else #(@direction == "E") 
+			@direction = "N"
+		end
 	end
-
-	def display_position
-		puts "#{@x}, #{@y}, #{@direction}"
+	
+	def turn_right
+		if (@direction == "N") 
+			@direction = "E"
+		elsif (@direction == "E") 
+			@direction = "S"
+		elsif (@direction == "S") 
+			@direction = "W"
+		else #(@direction == "W") 
+			@direction = "N"
+		end
 	end
 end
 
 rover1 = Rover.new(1,1, "N")
+rover1.display_position
+rover1.read_instruction
+rover1.interpret_instructions
 rover1.display_position
