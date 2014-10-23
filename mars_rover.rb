d2a2978821211318a2ec411fac1d8942e147f044
Vincent Jim <vjim323@gmail.com>
2014-10-23 14:42:39 -0400

    Displays rover's current position

--- mars_rover.rb
+++ mars_rover.rb
@@ -7,4 +7,22 @@ class Rover
 		@direction = direction
 	end
 
+	def read_instruction
+		
+	end
+
+	def move
+		#new_point = point.x + instruction.x, point.y + instruction.y, point.direction + instruction.direction
+	end
+
+	def turn
+		
+	end
+
+	def display_position
+		puts "#{@x}, #{@y}, #{@direction}"
+	end
 end
+
+rover1 = Rover.new(1,1, "N")
+rover1.display_position
